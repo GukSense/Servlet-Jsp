c1b0818c63a9a2d66c8151da0ecf7ac8367f1efd
Guk <101082667+GukSense@users.noreply.github.com>
2023-01-10 11:57:58 +0900

    Update UserDAO.java

--- UserDAO.java
+++ UserDAO.java
@@ -129,33 +129,136 @@ public class UserDAO {
 		return resultUser;
 	}
 	
-	public ArrayList<User> getEditUsers(){
-		ArrayList<User> editUsers = null;
+	public void EditUsers(User user){
+		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		
+		String idx = Integer.toString(user.getU_idx());
 		
 		try {
 			conn = DBConnection.getConnection();
-			String query = "select * from user";
+			String query = "update user set u_id=?, u_pw=?,u_name=?,u_tel=?,u_age=? where u_idx=?";
 			pstmt = conn.prepareStatement(query);
-			rs = pstmt.executeQuery();
-			editUsers = new ArrayList<>();
+			
+			pstmt.setString(1, user.getU_id());
+			pstmt.setString(2, user.getU_pw());
+			pstmt.setString(3, user.getU_name());
+			pstmt.setString(4, user.getU_tel());
+			pstmt.setString(5, user.getU_age());
+			pstmt.setString(6, idx);
+			pstmt.executeUpdate();	
+		
 			
 		} catch(SQLException e) {
 			e.printStackTrace();
+		} catch(ClassNotFoundException ex) {
+			ex.printStackTrace();
 		} finally {
 			try{
-				rs.close();
 				conn.close();
 				pstmt.close();
 			} catch(SQLException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+	public void deleteUser(User user) {
 		
+		String idx = Integer.toString(user.getU_idx());
+		Connection conn = null;
+		PreparedStatement pstmt = null;
 		
+		try {
+			String query = "delete from user where u_idx=?";
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(query);
+			
+			pstmt.setString(1, idx);
+			pstmt.executeUpdate();
+			
+		} catch(SQLException ex) {
+			ex.printStackTrace();
+		} catch(ClassNotFoundException ex) {
+			ex.printStackTrace();
+		} finally {
+			try{
+				if(conn != null) {conn.close();}
+				if(pstmt != null) {pstmt.close();}
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
 		
-		return editUsers;
+	}
+	
+	public int getUsersCount() {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		int count = 0;
+		
+		try {
+			String query = "SELECT COUNT(*) AS count FROM user ";
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+			while(rs.next()) {
+				count = rs.getInt("count");
+			}
+		} catch(Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if (rs != null) rs.close();
+				if (pstmt != null) pstmt.close();
+				if (conn != null) conn.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+			
+		}
+		
+		return count;
+	}
+	
+	public ArrayList<User> getUsers(int page) {
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<User> list = null;
+		
+		try {
+			String query = "select * from user limit ?,3";
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, page);
+			rs = pstmt.executeQuery();
+			
+			list = new ArrayList<>();
+			
+			while (rs.next()) {
+				User user = new User();
+				user.setU_idx(rs.getInt("u_idx"));
+				user.setU_id(rs.getString("u_id"));
+				user.setU_name(rs.getString("u_name"));
+				user.setU_tel(rs.getString("u_tel"));
+				user.setU_age(rs.getString("u_age"));
+				list.add(user);
+			}
+		} catch(SQLException e) {
+			e.printStackTrace();
+		} catch(ClassNotFoundException e) {
+			
+		} finally {
+			try {
+				if(rs != null) rs.close();
+				if(pstmt != null) pstmt.close();
+				if(conn != null) conn.close();
+			} catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
 	}
 }
